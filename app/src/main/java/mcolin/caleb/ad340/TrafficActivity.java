73b39bf0a7e7d7be6aa1d11f3deb6f66e0a76963
Caleb Mcolin <Caleb.mcolin@gmail.com>
2021-05-07 12:38:30 -0700

    Added the adapter to the RecyclerView

--- app/src/main/java/mcolin/caleb/ad340/TrafficActivity.java
+++ app/src/main/java/mcolin/caleb/ad340/TrafficActivity.java
@@ -2,61 +2,72 @@ package mcolin.caleb.ad340;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.Console;
+import java.util.ArrayList;
 
 public class TrafficActivity extends AppCompatActivity {
 
-
-    // Instantiate the RequestQueue.
-    RequestQueue requestQueue;
+    private RecyclerView recyclerView;
+    private RequestQueue requestQueue;
+    private ArrayList<CamItem> camItemArrayList;
+    private CamAdapter camAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_traffic);
 
-        // Initiate RecyclerView, put ListView into the ArrayAdapter
-        RecyclerView recyclerView = findViewById(R.id.trafficCams);
-        CamAdapter camsAdapter = new CamAdapter(this);
-        recyclerView.setAdapter(camsAdapter);
+        // Initiate RecyclerView
+        recyclerView = findViewById(R.id.trafficCams);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Initiate queue
+        // Initiate ArrayList of items
+        camItemArrayList = new ArrayList<>();
+
+        // Initiate RequestQueue from 'Volley' for loading data from external source
         requestQueue = Volley.newRequestQueue(this);
 
-        // Start 'Volley' to gather data
-        gatherData();
+        // Check network connection
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            gatherData();
+        }else{
+            Toast.makeText(getApplicationContext(), "No connection Detected.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
-     * Method used the Volley library to relieve JSON data from a given url.
+     * Method uses the Volley library to relieve JSON data from a given url.
      * The desired data is then parsed out in to string for use in the RecyclerView
      */
     public void gatherData() {
@@ -79,9 +90,12 @@ public class TrafficActivity extends AppCompatActivity {
                                 String address = cameraItem.getString("Description");
                                 String imageUrl = cameraItem.getString("ImageUrl");
                                 String type = cameraItem.getString("Type");
-
+                                // Pass the parsed data into the CamItem class
+                                camItemArrayList.add(new CamItem(id, type, address, imageUrl));
                             }
                         }
+                        camAdapter = new CamAdapter(TrafficActivity.this, camItemArrayList);
+                        recyclerView.setAdapter(camAdapter);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -90,40 +104,95 @@ public class TrafficActivity extends AppCompatActivity {
     }
 
     /**
+     * Inner class for each Camera item
+     */
+    public static class CamItem {
+        private final String mCamId;
+        private final String mCamType;
+        private final String mCameAddress;
+        private final String mCamImageUrl;
+
+        public CamItem(String id, String type, String address, String url) {
+            mCameAddress = address;
+            mCamId = id;
+            mCamType = type;
+            mCamImageUrl = url;
+        }
+
+        public String getCamId() {
+            return mCamId;
+        }
+
+        public String getType() {
+            return mCamType;
+        }
+
+        public String getAddress() {
+            return mCameAddress;
+        }
+
+        public String getUrl() {
+            return mCamImageUrl;
+        }
+    }
+
+    /**
      * Inner Adapter class for populating the RecyclerView
      */
     public static class CamAdapter extends RecyclerView.Adapter<CamAdapter.MyViewHolder> {
-        private final Context context;
+        private final Context mContext;
+        private final ArrayList<CamItem> camItemsList;
 
-        public CamAdapter(Context c) {
-            this.context = c;
-            LayoutInflater inflater = LayoutInflater.from(context);
+        public CamAdapter(Context c, ArrayList<CamItem> list) {
+            mContext = c;
+            camItemsList = list;
         }
 
+        @NonNull
         @Override
         public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//            View view = inflater.inflate(R.layout.custom_layout, parent, false);
-//            MyViewHolder holder = new MyViewHolder(view);
-            return null;
+            View v = LayoutInflater.from(mContext).inflate(R.layout.traffic_item, parent, false);
+            return new MyViewHolder(v);
         }
+
 
         @Override
         public void onBindViewHolder(MyViewHolder holder, int position) {
-//            holder.serial_number.setText(<your string array[position]>);
+            Context mContext = holder.mCamImage.getContext();
+            // Using 'position' to cycle through each item in the array
+            CamItem currentItem = camItemsList.get(position);
+            // Getting the data that was passed into the CamItem class
+            String url = currentItem.getUrl();
+            String id = currentItem.getCamId();
+            String type = currentItem.getType();
+            String address = currentItem.getAddress();
+            // Setting the data to the items in the 'traffic_item.xml' using the holder below
+            holder.mCamId.setText(id + " : ");
+            holder.mCamType.setText(type);
+            holder.mCamAddress.setText(address);
+            Picasso.get().load(url).fit().centerInside().into(holder.mCamImage);
         }
 
         @Override
         public int getItemCount() {
-//            return <size of your string array list>;
-            return 0;
+            return camItemsList.size();
         }
 
-        class MyViewHolder extends RecyclerView.ViewHolder {
-            TextView serial_number;
+        /**
+         * Inner ViewHolder Class with in the RecyclerView Adapter
+         */
+        static class MyViewHolder extends RecyclerView.ViewHolder {
+            public ImageView mCamImage;
+            public TextView mCamId;
+            public TextView mCamType;
+            public TextView mCamAddress;
 
             public MyViewHolder(View itemView) {
                 super(itemView);
-//                serial_number = (TextView) itemView.findViewById(R.id.serialNo_CL);
+                mCamImage = itemView.findViewById(R.id.cam_image);
+                mCamId = itemView.findViewById(R.id.cam_id);
+                mCamType = itemView.findViewById(R.id.cam_type);
+                mCamAddress = itemView.findViewById(R.id.cam_address);
             }
         }
     }
