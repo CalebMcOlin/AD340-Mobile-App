2251067db8ab8e6c8e5622b2bcd2069b1de43331
Caleb Mcolin <Caleb.mcolin@gmail.com>
2021-06-11 23:36:00 -0700

    Revert "signin method started (BROKEN)"
    This reverts commit c9ab506e8438558aba70ef7b07d1d2d60aecbefb.

--- app/src/main/java/mcolin/caleb/ad340/MainActivity.java
+++ app/src/main/java/mcolin/caleb/ad340/MainActivity.java
@@ -3,22 +3,12 @@ package mcolin.caleb.ad340;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 
 import java.util.Objects;
 
@@ -45,41 +35,6 @@ public class MainActivity extends BaseActivity {
         mSendBtn.setOnClickListener(new BtnOnClickListener());
     }
 
-    private void signIn() {
-        Log.d("FIREBASE", "signIn");
-        // 1 - TODO validate display name, email, and password entries
-
-        // 2 - TODO save valid entries to shared preferences
-
-        // 3 - sign into Firebase
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                Log.d("FIREBASE", "signIn:onComplete:" + task.isSuccessful());
-                if (task.isSuccessful()) {
-                    // update profile. username is the value entered in UI
-                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().setDisplayName(username).build();
-                    user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                Log.d("FIREBASE", "User profile updated.");
-                                // Go to FirebaseActivity
-                                startActivity(new Intent(MainActivity.this, FirebaseActivity.class));
-                            }
-                        }
-                    });
-                } else {
-                    Log.d("FIREBASE", "sign-in failed");
-                    Toast.makeText(MainActivity.this, "Sign In Failed",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
     /**
      * Inner Button Click listener Class
      */
@@ -95,12 +50,9 @@ public class MainActivity extends BaseActivity {
             } else if (btn.getText() == "Traffic Cams") {
                 Intent intent = new Intent(getBaseContext(), TrafficActivity.class);
                 startActivity(intent);
-            } else if ((btn.getText() == "Locations")) {
+            } else if ((btn.getText() == "Locations")){
                 Intent intent = new Intent(getBaseContext(), LocationActivity.class);
                 startActivity(intent);
-            } else if ((btn.getText() == "Login")) {
-                Toast.makeText(getApplicationContext(), "Logging in...", Toast.LENGTH_SHORT).show();
-                signIn();
             } else {
                 Toast.makeText(getApplicationContext(), btn.getText(), Toast.LENGTH_SHORT).show();
             }
